0f4131b5c967c68ae2dd4b2f6b7d249c3052e605
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:37 -0500

    xml commenting

--- src/Soenneker.ProductBoard.OpenApiClientUtil/Abstract/IProductBoardOpenApiClientUtil.cs
+++ src/Soenneker.ProductBoard.OpenApiClientUtil/Abstract/IProductBoardOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.ProductBoard.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IProductBoardOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<ProductBoardOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
